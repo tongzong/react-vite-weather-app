f47233f7ea4947993b10d57a6c06fd29f37450d1
Bitrus Sunday Gomam <sbitrus61@gmail.com>
2025-05-05 10:04:04 +0100

    url edited from http to https

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -30,7 +30,7 @@ const Weather = () => {
       return;
     }
     try {
-      const url = `http://api.weatherapi.com/v1/current.json?&key=${
+      const url = `https://api.weatherapi.com/v1/current.json?&key=${
         import.meta.env.VITE_APP_ID
       }&q=${city}`;
 
